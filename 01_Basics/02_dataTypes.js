089a57fe07fabc1033c3d8bdbcf481d2eb81655d
Divya Anand <121435540+Divue@users.noreply.github.com>
2024-01-05 08:49:47 +0000

    data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version of js
+
+// alert("hello")    //we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+let name="hitesh"      //string
+let age=10             // int 
+let isLoggedIn = false // boolean
+let state;
+
+// number range => 2^53
+// bigint
+// string =>""
+// boolean => true/false
+
+// numm = standalone value      empty,value not assigned
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "divue")        // strign
+console.log(typeof null);         // object
+console.log(typeof undefined);   // undefined
