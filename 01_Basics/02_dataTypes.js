835452f028e1a210b1f82f755f953b3c4c128b1e
Divya Anand <121435540+Divue@users.noreply.github.com>
2024-01-05 15:00:12 +0000

    primitive and non primitive data type + typeof data type etc.....

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -6,6 +6,30 @@ console.log(3
     +
     3) // code readability should be high
 
+
+//Primitive data type
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference (Non Primitive)
+// Arrays, Objects, Functions
+ 
+const heros=["shaktiman", "naagraj", "doga"]  //array
+
+let myObj={
+    name:"Divue",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("HEllo World");
+}
+
+console.log(typeof myObj)
+
+
+
+
 let name="hitesh"      //string
 let age=10             // int 
 let isLoggedIn = false // boolean
