520a404aeb7c8373426a3b9adda860feab00a3b4
Divya Anand <121435540+Divue@users.noreply.github.com>
2024-01-08 20:41:55 +0000

    functions and arrays

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -33,3 +33,37 @@ function loginUserMsg(username){
 loginUserMsg("hitesh")  // nothing will happen
 console.log(loginUserMsg("hitesh")) //will print jitesh just logged in
 console.log(loginUserMsg())   //prints undefined just logged in 
+console.log();
+
+
+
+function calculateCartPrice(...num1) {  //rest and spred operator here it is used as rest operator and will return array [ 2, 200, 400, 500 ]
+    return num1
+}
+console.log(calculateCartPrice(2, 200, 400, 500))
+
+
+function calculateCaret( val1,val2, ...num1){
+    return num1
+}
+console.log(calculateCaret(200, 400, 500, 20000));   // will return this coz 200 and 400 are taken as val1 and val2 and the rest are taken as num1 through rest operator[ 500, 20000 ]
+
+const user={
+    username:"hitesh",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`User name is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user)
+
+handleObject({
+    username:"sam",
+    price: 300
+})
+
+const myNerArray = [200, 500, 100 ,400]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNerArray));  //print 500
