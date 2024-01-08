deaa961c35ad6b64da8651ac7ad97bafd4f18597
Divya Anand <121435540+Divue@users.noreply.github.com>
2024-01-08 08:25:50 +0000

    object part 1

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,39 @@
+// singleton 
+// Object.create    //constructor method known as singleton 
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const JsUser={
+    name:"hitesh",      //name age location is taken as string 
+    "full_name": "Hitesh Choudhary",  //full name cannot be accessed using .
+    [mySym]: "mykey1",    //to use symbol [] is needed  to show it is a symbol key
+    age: 18,
+    location: "jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDaya:["monday", "tues"]
+}
+
+console.log((JsUser.email));
+console.log(JsUser["email"]);
+console.log(JsUser.full_name);
+console.log(JsUser[mySym]);
+
+JsUser.email="hitchat@gpt.com"
+// Object.freeze(JsUser)  //it will freeze the value and will not let it change in future
+// JsUser.email="hit@dsg.com"  
+// console.log(JsUser)
+
+
+JsUser.greeting = function(){
+    console.log("hello js user");
+}
+
+JsUser.gree2= function(){
+    console.log(`heelo js user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.gree2());
