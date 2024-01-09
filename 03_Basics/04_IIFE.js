37cacbf594cf7856b5c2b08e3d7f1263ce9da8e4
Divya Anand <121435540+Divue@users.noreply.github.com>
2024-01-09 15:27:39 +0000

    IIFE Immediately INvoked Function Expression named and unnamed iife

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,17 @@
+// Immediately Invoked Function Expressions (IIFE)
+// used to get rid of global scope pollution
+
+// function chai(){
+//     console.log(`DB CONNECTED`);
+// }
+// chai()
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();      // will work     in IIFE ; is needed to end
+
+( (name) =>{
+    //unnamed IIFE
+    console.log(`db conneced ${name} `);
+})('hitesh');      // ; is needed to end 
